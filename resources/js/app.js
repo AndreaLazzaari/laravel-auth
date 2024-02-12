4e525e0ece0f86e484bb5eb6e4559975b6161695
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-12 16:20:35 +0100

    implementato nuovo progetto inserendo laravel ui + l'autoidentificazione

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
-import '~resources/scss/app.scss';
+// import '~resources/scss/app.scss';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 
 // per far comprendere le img che importo a vite
 import.meta.glob([
