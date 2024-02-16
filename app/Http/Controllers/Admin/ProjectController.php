8dd6bba4f00edb86bb1f836fdc08a61ce3ef4bb4
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-16 17:11:40 +0100

    importata la crud index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return 'index dei project';
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
